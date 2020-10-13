fe71b81b6ea081f3156f9ee6e03ffd9ed20b1ab9
TkTioNG <tktiong@icloud.com>
2020-10-13 18:12:35 +0800

    base models have constructed

--- breeds/models.py
+++ breeds/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Breed(models.Model):
+    """
+    Type of the cat breed.
+    name - PK: Name of the breed.
+    origin: Origin of the cat breed.
+    description: description on the cat breed.
+    """
+    pass
+
+
+class Human(models.Model):
+    """
+    People who breed a cat.
+    name: Name of the person.
+    gender: gender of the person.
+    date_of_birth: birth date of the person.
+    home - FK: house of the person.
+    """
+    pass
+
+
+class Home(models.Model):
+    """
+    House of a person.
+    name: Name of the house? TODO: to be confirm
+    address: address of the house.
+    type: type of the house (landed|condominium)
+    """
+    pass
+
+class Cat(models.Model):
+    """
+    Cat.
+    name: Name of the cat.
+    gender: gender of the cat.
+    date_of_birth: birth date of the cat
+    breed - FK: Type of the cat breed
+    owner - FK: Owner to the cat
+    """
+    pass
