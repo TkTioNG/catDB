ca4dedf10589fb6a441ecbb63eb16e906f7ddfc3
TkTioNG <tktiong@icloud.com>
2020-10-15 17:46:24 +0800

    Update expiring token

--- catDB/settings.py
+++ catDB/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'breeds.authentication.ExpiringTokenAuthentication',
     ]
 }
